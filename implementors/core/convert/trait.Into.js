f76769d1e09fb94c8f5c1a68501ce2cc3f2bdd68
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-22 20:23:12 +0000

    Deploy nebgnahz/cv-rs to github.com/nebgnahz/cv-rs.git:gh-pages

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cv"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"cv/features2d/struct.MSER.html\" title=\"struct cv::features2d::MSER\">MSER</a>&gt; for <a class=\"struct\" href=\"cv/features2d/struct.MSERBuilder.html\" title=\"struct cv::features2d::MSERBuilder\">MSERBuilder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
